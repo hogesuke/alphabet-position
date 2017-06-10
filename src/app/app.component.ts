46cc39f53914e90707b0e771270eec899101f549
hogesuke <miyado@gmail.com>
2017-06-10 16:02:19 +0900

    完成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  characters: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  @Input() targets: string;
+
+  isActive (ch: string) {
+    return _(this.targets).words(/./g).includes(ch);
+  }
 }
